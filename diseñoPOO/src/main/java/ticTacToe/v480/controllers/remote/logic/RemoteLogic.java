4160d562248e9a2dd15cdaf94aca145b388e457f
tohmdat <tohmdat@gmail.com>
2019-06-10 15:36:32 +0200

    Add files via upload

---
+++ diseñoPOO/src/main/java/ticTacToe/v480/controllers/remote/logic/RemoteLogic.java
@@ -0,0 +1,30 @@
+package ticTacToe.v480.controllers.remote.logic;
+
+import ticTacToe.v480.Logic;
+import ticTacToe.v480.controllers.OperationController;
+import ticTacToe.v480.models.Observer;
+
+public class RemoteLogic implements Logic, Observer {
+
+	@Override
+	public void initialize() {
+	}
+
+	@Override
+	public void begin() {
+	}
+
+	@Override
+	public void end() {
+	}
+
+	@Override
+	public void exit() {		
+	}
+	
+	@Override
+	public OperationController getOperationController() {
+		return null;
+	}
+
+}
